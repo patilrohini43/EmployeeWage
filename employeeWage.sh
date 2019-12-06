6598fbd6623933ae7d353c18328f33289206e819
rohini <patilrohini43@gmail.com>
2019-12-06 18:36:41 +0530

    Added:calculate daily employee wage

--- employeeWage.sh
+++ employeeWage.sh
@@ -1,11 +1,16 @@
 #!/bin/bash/ -x
 echo "Welcome to Employee"
 employeeCheck=$(( RANDOM % 2 ))
-
+WagePerHrs=20
+WorkHrs=8
 if [[ $employeeCheck -eq 1 ]]
 then
+
 	echo "Employee Present"
+   DailyWage=$(( $WagePerHrs * $WorkHrs ))
+	echo $DailyWage
 else
 	echo "Employee Absent"
 fi
+
 
