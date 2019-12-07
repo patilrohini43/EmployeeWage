bb7cfe97b7ce4af4ecaea1843331b4e0455160fb
rohini <patilrohini43@gmail.com>
2019-12-07 12:37:53 +0530

    Added:working day per month

--- employeeWage.sh
+++ employeeWage.sh
@@ -1,27 +1,29 @@
 #!/bin/bash/ -x
 echo "Welcome to Employee"
-employeeCheck=$(( RANDOM % 3 ))
+
 WagePerHrs=20
 WorkDay=20
 FullWorkHrs=8
 HalfWorkHrs=4
+Day=1
 FullDay=1
 HalfDay=0
-echo $employeeCheck
+while [[ $day -le $WorkDay ]]
+do
+employeeCheck=$(( RANDOM % 3 ))
 case $employeeCheck in
 		$FullDay)
 			echo "Employee Present FullTime"
 			 DailyWage=$(( $WagePerHrs * $FullWorkHrs ))
-			 MonthlyWage=$(( $WorkDay * $DailyWage ))
       	 echo $DailyWage
-			 echo $MonthlyWage ;;
+			 ;;
 		$HalfDay)
 			 echo "Employee Present HalfTime"
 			 DailyWage=$(( $WagePerHrs * $HalfWorkHrs ))
-			 MonthlyWage=$(( $WorkDay * $DailyWage ))
       	 echo $DailyWage
-			 echo $MonthlyWage  ;;
+			  ;;
 		*)
 			echo "Employee absent"
 esac
-
+day=$(( $day + 1))
+done
