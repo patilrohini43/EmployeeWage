74703ca99d7d01a89d510f52d8053838950f26c5
rohini <patilrohini43@gmail.com>
2019-12-07 13:12:32 +0530

    Added:checking hours and day

--- employeeWage.sh
+++ employeeWage.sh
@@ -5,25 +5,30 @@ WagePerHrs=20
 WorkDay=20
 FullWorkHrs=8
 HalfWorkHrs=4
+Hours=0
 Day=1
 FullDay=1
 HalfDay=0
-while [[ $day -le $WorkDay ]]
+while [[ $day -le $WorkDay && $Hours -lt 100 ]]
 do
 employeeCheck=$(( RANDOM % 3 ))
 case $employeeCheck in
 		$FullDay)
 			echo "Employee Present FullTime"
 			 DailyWage=$(( $WagePerHrs * $FullWorkHrs ))
-      	 echo $DailyWage
+			 Hours=$(( $Hours + $FullWorkHrs ))
+			 echo $DailyWage
 			 ;;
 		$HalfDay)
 			 echo "Employee Present HalfTime"
 			 DailyWage=$(( $WagePerHrs * $HalfWorkHrs ))
-      	 echo $DailyWage
+			 Hours=$(( $Hours + $HalfWorkHrs))
+			echo $DailyWage
 			  ;;
 		*)
 			echo "Employee absent"
 esac
 day=$(( $day + 1))
 done
+
+
